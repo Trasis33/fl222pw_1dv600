eaced00f10e288403a61cad5c23af5a21a8aa550
Slangeman <fredrik.langa@gmail.com>
2019-02-22 09:41:17 +0100

    displays underlines for each letter

--- src/hangman.js
+++ src/hangman.js
@@ -1,14 +1,25 @@
 let word = require('./word')
+let wordLetters = require('./letter')
 
 // let words
 // let letters = this.word.split('')
-// let guessedLetters = []
+let guessedLetters = []
+let currentLettersArr = []
 // let guesses = guesses
 // let noMoreGuesses = false
 // let correctGuess = false
 
 let startGame = () => {
-  console.log(word.getWord())
+  let currentWord = word.getWord()
+  // console.log(currentWord)
+  let letters = currentWord.split('')
+
+  letters.forEach(letter => {
+    console.log(letter)
+    let currentLetters = wordLetters.showLetter(letter, false)
+    currentLettersArr.push(currentLetters)
+  })
+  console.log(currentLettersArr)
 }
 
 module.exports.startGame = startGame
